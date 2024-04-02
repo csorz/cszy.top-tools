7cc8531bd3f866469497dd7f2b716513a096a6f3
csorz <csorz@qq.com>
2024-04-02 15:28:16 +0800

    feat: 更新依赖

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -6,12 +6,18 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
   ],
   server: {
+    host: '0.0.0.0',
     port: 4000
+  },
+  build: {
+    // 最低支持64 https://vitejs.cn/vite3-cn/config/build-options.html
+    target: ['chrome64']
   },
   resolve: {
     alias: {
