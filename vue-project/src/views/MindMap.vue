b2bb2f6749605e4f0873dab8549f9dc382a4e093
csorz <csorz@qq.com>
2024-10-14 09:37:59 +0800

    feat: 更新思维导图生成图片demo

--- vue-project/src/views/MindMap.vue
+++ vue-project/src/views/MindMap.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import MindMap from "simple-mind-map";
+import * as dataJson from "../assets/mind-map.json"
+import Export from 'simple-mind-map/src/plugins/Export.js'
+// import Export from 'simple-mind-map/src/Export.js' Use this path for versions below v0.6.0
+import * as curriculumsJson from "../assets/curriculums.json"
 
-onMounted(() => {
+MindMap.usePlugin(Export)
+
+console.log('dataJSON', dataJson)
+
+function conversion() {
+  curriculumsJson.map(() => {
+    
+  })
+}
+
+onMounted(async () => {
   const mindMap = new MindMap({
-    el: document.getElementById("mindMapContainer"),
-    data: {
-      data: {
-        text: "根节点",
-      },
-      children: [],
-    },
+    el: document.getElementById("mindMapContainer")
   } as any);
+
+  if (dataJson.root) {
+    mindMap.setFullData(dataJson)
+  } else {
+  // 否则使用setData方法导入
+    mindMap.setData(dataJson)
+  }
+  // 导入数据后有可能新数据渲染在可视区域外了，所以为了更好的体验，可以复位一下视图的变换
+  mindMap.view.reset();
+
+  // 确保渲染完毕后生成图片base64
+  mindMap.render(async () => {
+    const imgRes = await mindMap.export('png', false)
+    console.log('imgRes', imgRes)
+  })
 })
 </script>
 
@@ -23,7 +46,7 @@ onMounted(() => {
 
 <style scoped>
 #mindMapContainer {
-  width: 720px;
+  width: 100%;
   height: 720px;
 }
 #mindMapContainer * {
